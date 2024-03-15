22987e573b5ea81a3d3230efb7316c0697b767b4
Adrien Gantner <adrien.gantner@gmail.com>
2024-03-15 12:03:33 +0100

    add logo and footer in templates

--- kirby-starterkit/site/snippets/footer.php
+++ kirby-starterkit/site/snippets/footer.php
@@ -10,6 +10,24 @@
 */
 ?>
 
+<footer>
+    <p id="footer-title"><a href="../../index.html"><u>Typotes Club</u></a></p>
+    <li>
+        <ul>
+            <a href="../../index.html#moveable">Fontes</a>
+        </ul>
+        <ul>
+            <a href="../../licences/index.html">Licences</a>
+        </ul>
+        <ul>
+            <a href="">À propos</a>
+        </ul>
+        <ul>
+            <a id="link-footer" target="_blank" href="https://lacambretypo.be/fr">Atelier&nbsp;de&nbsp;typographie&nbsp;La&nbsp;Cambre</a>
+        </ul>
+    </li>
+</footer>
+
   <?= js([
     'assets/js/prism.js',
     'assets/js/lightbox.js',
